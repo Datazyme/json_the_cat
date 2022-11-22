20971202b7759d34f12bdb54744b7f77cd387a73
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-22 05:19:18 +0000

    now with callbacks, returns and exported

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,27 @@
 const request = require('request');
-const args = process.argv.slice(2);
-let breed = args[0];
+//const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds', (error, response, body) => {
+    if (error) return callback(error, null);
+      const data = JSON.parse(body);//turns array into object
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCose} when fetching breed: ${body}`), null);
+      return;
+    };
+    //looks through each object in array and pulls out description for node argument of breed ex "Siberian". breed is capitalized
+    for (let i in data) {
+      if (data[i].name === breedName) {
+        desc = data[i].description
+        callback(null, desc);
+        return;
+      };
+    };
+  //have to use callback here with string inside. The return at the end like that ends the if statement
+    if(data.name !== breedName) {
+    callback("Breed not found");
+    return;
+   }
+  });
+};
+module.exports = {fetchBreedDescription}
 
-//returns description of cat breed enetered by user. Searches through api below
-request('https://api.thecatapi.com/v1/breeds', (error, response, body) => {  
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);//turns array into object
-  //looks through each object in array and pulls out description for node argument of breed ex "Siberian". breed is capitalized
-  for (let i in data) {
-    if (data[i].name === breed) {
-      console.log(data[i].description);
-    }
-  }
-  //put this if here to get all the i's checked and then print message if breed is not found
-  if (data.name !== breed) {
-    console.log("Breed not found");
-  }
-});
