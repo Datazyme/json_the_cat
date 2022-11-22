c64e7e269fbc329b2e779a5d2028cd2ed46d9c51
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-22 06:20:53 +0000

    now with eslinting

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,25 +3,25 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds', (error, response, body) => {
     if (error) return callback(error, null);
-      const data = JSON.parse(body);//turns array into object
+    const data = JSON.parse(body);//turns array into object
     if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCose} when fetching breed: ${body}`), null);
       return;
-    };
+    }
     //looks through each object in array and pulls out description for node argument of breed ex "Siberian". breed is capitalized
     for (let i in data) {
       if (data[i].name === breedName) {
-        desc = data[i].description
-        callback(null, desc);
+        //desc = data[i].description;
+        callback(null, data[i].description);
         return;
-      };
-    };
-  //have to use callback here with string inside. The return at the end like that ends the if statement
-    if(data.name !== breedName) {
-    callback("Breed not found");
-    return;
-   }
+      }
+    }
+    //have to use callback here with string inside. The return at the end like that ends the if statement
+    if (data.name !== breedName) {
+      callback("Breed not found");
+      return;
+    }
   });
 };
-module.exports = {fetchBreedDescription}
+module.exports = {fetchBreedDescription};
 
